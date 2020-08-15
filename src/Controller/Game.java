69b5ce7022f3e199965c47bee36054123e0b4f66
Chi Thuan <chithuandev@gmail.com>
2020-08-15 14:10:24 +0700

    thuan fix new game - chua xong

--- src/Controller/Game.java
+++ src/Controller/Game.java
@@ -22,6 +22,7 @@ import Model.SoundPlayer;
 import Model.SpaceCraft;
 import Model.SpaceCraftWeapon;
 import View.GameMenu;
+import View.GameView;
 
 import java.applet.Applet;
 import java.applet.AudioClip;
@@ -130,6 +131,9 @@ public class Game extends JPanel implements ActionListener {
             	}else superBuff=0;
                    
             }
+            if(isGameLost == true && e.getKeyCode() == KeyEvent.VK_SPACE) {
+            	System.exit(0);
+            }
         }
 
         @Override
@@ -213,6 +217,7 @@ public class Game extends JPanel implements ActionListener {
                 playGameSound.playMusicBackground();
               
             }
+            
             if (mouseX > menu.settingsButton().x && mouseX < menu.settingsButton().x + menu.settingsButton().width &&
                 mouseY > menu.settingsButton().y && mouseY < menu.settingsButton().y + menu.settingsButton().height &&
                     !isGameStarted && menu.isMainMenuActive()){
@@ -280,7 +285,7 @@ public class Game extends JPanel implements ActionListener {
 
         time = new Timer(2, this);
         time.start();
-      backgroundSound.play();
+        backgroundSound.play();
     }
 
     /**
@@ -686,7 +691,11 @@ public class Game extends JPanel implements ActionListener {
             if (isGameLost){
                 graphics2D.setFont(new Font("SanSerif", Font.BOLD, 50));
                 graphics2D.setColor(Color.red);
-                graphics2D.drawString("YOU SUCK", (screenWidth - 200) / 2, screenHeight / 2);
+                graphics2D.drawString("YOU LOSS", (screenWidth - 200) / 2, screenHeight / 3);
+                graphics2D.setFont(new Font("SanSerif", Font.BOLD, 16));
+                graphics2D.setColor(Color.GRAY);
+                graphics2D.drawString("Enter to continute", (screenWidth - 95) / 2, screenHeight / 2);
+                addKeyListener(new MyActionListener());
             }
         }
     }
